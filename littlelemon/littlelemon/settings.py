bf44c966cacb5db77db4fb940f6d6f13584e7556
houssemkoubaa <houssem.koubaa2000@gmail.com>
2023-06-19 12:17:32 +0100

    models

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'restaurant',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -84,7 +85,7 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-            }
+        }
     }
 }
 
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,7 +1,8 @@
-#define URL route for index() view
+# define URL route for index() view
 from django.urls import path
-from . import views
 
+from .views import MenuItemsView, SingleMenuItemView
 urlpatterns = [
-    path('', views.index, name='index')
+    path('menu/', MenuItemsView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view()),
 ]
